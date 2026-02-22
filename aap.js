567fc62500ee0da8a3cd68f72f886ca96b81b31f
Devendra8871 <167069533+Devendra8871@users.noreply.github.com>
2026-02-22 17:05:40 +0530

    Add initial HTML, CSS, and JavaScript files for music player project

---
+++ aap.js
@@ -0,0 +1,21 @@
+// let n = prompt("Enter a number");
+// n=parseInt(n);
+// for(let i=n;i<=n*10;i=i+n){
+//      console.log(i);
+// }
+
+//  for(let i=1;i<=3;i++)
+//     {
+//       for(let j=1;j<=3;j++)
+//         {
+//            console.log(i+j);
+//           }
+//     }
+
+// const favMovie = "avatar";
+// let guess = prompt("Guess the movie name");
+//  while(guess != favMovie && guess != "quit"){
+//     guess = prompt("wrong guess");
+//  }
+        
+
